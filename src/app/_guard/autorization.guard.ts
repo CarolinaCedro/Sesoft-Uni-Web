97304daa0ef3f8709c5a3573562b026b6c19d8fc
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2023-10-08 18:49:40 -0300

    [SESUNI-01] - feat: creating Autorization Guard

---
+++ src/app/_guard/autorization.guard.ts
@@ -0,0 +1,6 @@
+import { CanActivateFn } from '@angular/router';
+import { getFromLocalStorage } from 'src/utils/local-storage.util';
+
+export const autorizationGuard: CanActivateFn = (route, state) => {
+  return !!getFromLocalStorage('token');
+};
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from "./components/sign-in/sign-in.component";
 import { HomeComponent } from "./components/home/home.component";
 import { SignUpComponent } from "./components/sign-up/sign-up.component";
+import { autorizationGuard } from './_guard/autorization.guard';
 
 const routes: Routes = [
   { path: "", component: SignInComponent },
-  { path: "home", component: HomeComponent },
+  { path: "home", component: HomeComponent, canActivate: [autorizationGuard] },
   { path: "sign-up", component: SignUpComponent },
   { path: "**", component: HomeComponent },
 ];
