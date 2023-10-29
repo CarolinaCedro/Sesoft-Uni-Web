1e6d650a79fedbab495b6189699df228c1baaca4
CarolinaCedro <yotechgirl@gmail.com>
2023-10-29 18:08:02 -0300

    feat: ajustando conflitos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,11 +26,11 @@ import { ExplorerComponent } from './components/explorer/explorer.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { NotificationComponent } from './components/notification/notification.component';
 import { SettingsComponent } from './components/settings/settings.component';
-import { FeedComponent } from './components/home/feed/feed.component';
 import { MatCardModule } from "@angular/material/card";
 import { TokenInterceptor } from './interceptors/token.interceptor';
 import { PostNotificationService } from './components/listeners/post-notification-service.service';
 import { SearchComponent } from './components/search/search.component';
+import {FeedComponent} from "./components/home/feed/feed/feed.component";
 
 
 @NgModule({
