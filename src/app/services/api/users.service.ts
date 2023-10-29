8ca61c1af9faa15027a3c7e7b1b4c02ae97fae41
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2023-10-28 21:15:20 -0300

    feat: url api

--- src/app/services/api/users.service.ts
+++ src/app/services/api/users.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Router } from "@angular/router";
-import { catchError, Observable, tap, throwError } from "rxjs";
-import { getFromLocalStorage } from 'src/utils/local-storage.util';
+import { Observable } from "rxjs";
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private readonly endpoint = 'users'
-
   constructor(private http: HttpClient, private route: Router) { }
 
   findUsers(searchQuery: string): Observable<any> {
     const params = {
       search: searchQuery
     };
+    const endpoint = 'users'
 
-    return this.http.get(environment.apiUrl + this.endpoint, { params }).pipe()
+    return this.http.get(environment.apiUrl + endpoint, { params }).pipe()
+  }
+
+  getMe(): Observable<any> {
+    const endpoint = 'users/me'
+
+    return this.http.get(environment.apiUrl + endpoint).pipe()
   }
 }
