2984919320575f0a08404d71d38286f1ff2bceeb
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2023-10-28 21:19:32 -0300

    fix/refact: profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { UserService } from 'src/app/services/api/users.service';
+
+interface UserProfile {
+  id: string;
+  username: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  likesCount: number;
+  postsCount: number;
+  followingsCount: number;
+  followersCount: number;
+  profile: {
+    id: string;
+    displayName: string;
+    bio: string | null;
+    websiteUrl: string | null;
+    createdAt: string;
+    updatedAt: string;
+    iconStorageId: string | null;
+    icon: string | null;
+  };
+}
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +29,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  activeTab: string = 'postagens';
+  loading: boolean = false;
+  user: UserProfile = {} as UserProfile;
 
+  constructor(
+    private readonly renderer: Renderer2,
+    private readonly service: UserService
+  ) { }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.service.getMe().subscribe(
+      res => {
+        this.user = res;
+        this.loading = false;
+      });
+  }
+
+  switchTab(tab: string) {
+    this.activeTab = tab;
+
+    const tabButtons = document.querySelectorAll('.tab-btn');
+    tabButtons.forEach((button) => {
+      this.renderer.removeClass(button, 'active');
+    });
+    const activeButton = document.querySelector(`[data-tab=${tab}]`);
+    this.renderer.addClass(activeButton, 'active');
+
+    const tabContents = document.querySelectorAll('.tab-content');
+    tabContents.forEach((content) => {
+      this.renderer.removeClass(content, 'active');
+    });
+    const activeContent = document.querySelector(`#${tab}`);
+    this.renderer.addClass(activeContent, 'active');
+  }
 }
