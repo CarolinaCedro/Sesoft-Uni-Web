3f95414bed1def5116f7eed9b8a40b265854edc7
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2023-11-10 16:52:40 -0300

    fix: api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000/'
-  // apiUrl: 'https://sesoft-uni-backend-development.up.railway.app/'
+  // apiUrl: 'http://localhost:4000/'
+  apiUrl: 'https://sesoft-uni-backend-development.up.railway.app/'
 };
