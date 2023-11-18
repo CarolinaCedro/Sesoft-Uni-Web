3c8a35664fed5dd649c6e7ac888a0eff5d8c1b03
CarolinaCedro <yotechgirl@gmail.com>
2023-11-18 14:01:25 -0300

    refactoring: refactoring view explorar

--- src/app/components/explorer/explorer.component.ts
+++ src/app/components/explorer/explorer.component.ts
@@ -9,42 +9,25 @@ import {User} from "../../interfaces/user.models";
 })
 export class ExplorerComponent implements OnInit {
 
-  user: User[] = []
-
-
-
-  events = [
-    {
-      title: 'Evento 1',
-      date: '2023-10-25',
-      location: 'Local 1',
-      description: 'Descrição do Evento 1'
-    },
-    {
-      title: 'Evento 2',
-      date: '2023-11-15',
-      location: 'Local 2',
-      description: 'Descrição do Evento 2'
-    },
-    {
-      title: 'Evento 3',
-      date: '2023-12-05',
-      location: 'Local 3',
-      description: 'Descrição do Evento 3'
-    }
-  ];
+  users: User[] = []
 
   constructor(private userService: UserService) {
   }
 
   ngOnInit(): void {
+    this.getMyFollowers()
   }
 
   getMyFollowers() {
-    this.userService.getFollowingUsers().subscribe(
+    this.userService.getAllUsers().subscribe(
       res => {
         console.log("res", res)
+        this.users = res?.result
       }
     )
+  }
+
+  onFollow(item: any) {
+    console.log("teste")
   }
 }
