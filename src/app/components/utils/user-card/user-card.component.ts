617c2f62e7065989fd61adb791127a3ab1ff3b2d
CarolinaCedro <yotechgirl@gmail.com>
2023-11-18 16:12:10 -0300

    feat: trazendo os followers no perfil também

--- src/app/components/utils/user-card/user-card.component.ts
+++ src/app/components/utils/user-card/user-card.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from "../../../interfaces/user.models";
+import {UserService} from "../../../services/api/users.service";
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss']
 })
-export class UserCardComponent {
+export class UserCardComponent implements OnInit {
   @Input() profilePicture: string | undefined = '';
   @Input() displayName: string = '';
   @Input() followersCount: number = 0;
@@ -15,4 +17,64 @@ export class UserCardComponent {
   @Input() postsCount: number = 0;
   @Input() posts: any;
   @Input() likedPosts: any;
+
+
+  users: User[] = []
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.getMyAllFollowers()
+    this.getStatusOnBtn()
+  }
+
+  getMyAllFollowers() {
+    this.userService.getFollowingUsers().subscribe(
+      res => {
+        console.log("res", res)
+        this.users = res?.result
+      }
+    )
+  }
+
+  getStatusOnBtn() {
+    this.userService.getFollowingUsers().subscribe(
+      res => {
+        console.log("todos os usuários", res);
+        const allUsers = res?.result;
+
+        this.userService.getFollowingUsers().subscribe(
+          followingUsers => {
+            console.log("usuários que estou seguindo", followingUsers?.result);
+
+            // Filtrando os usuários pra pegar só o que eu n sigo
+            this.users = allUsers.map((user: User) => {
+              const isFollowing = followingUsers?.result.some((followingUser: {
+                id: any;
+              }) => followingUser.id === user.id);
+              return new User(user.id, user.email, user.profile, user.username, isFollowing);
+            });
+
+          }
+        );
+      }
+    );
+  }
+
+  onFollow(user: User) {
+    this.userService.follow(user.id).subscribe(
+      res => {
+        user.following = true
+      }
+    );
+  }
+
+  unFollow(user: User) {
+    this.userService.unfollow(user.id).subscribe(
+      res => {
+        user.following = false
+      }
+    );
+  }
 }
